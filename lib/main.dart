ef857fccc34c19a9a5a79de9d91e736f8b49baca
Benjamin <benjanuva@aol.com>
2020-05-16 16:16:46 -0400

    -created scaffold and appBar -created stateless widget -created stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+          home: BallPage()),
+      );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[700],
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+  resetBall() {}
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int  ballNumber = 0;
+
+  changeBallNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+      print('clicked');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          changeBallNumber();
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
+  }
+}
